d24e42832ca3eeb6b600762b75789a668662228e
Jason Mueller <jason@suspendedgravity.com>
2014-09-11 03:01:11 -0500

    Update sample script to work with new resolve

--- Source/Examples/DeployPackage.ps1
+++ Source/Examples/DeployPackage.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 param (
     [String]
     [Parameter(Position = 0, Mandatory = $true)]
@@ -15,27 +16,51 @@ param (
     [Parameter(Position = 3, Mandatory = $true)]
     $SettingsPath
 )
+$verbose = $PSBoundParameters['Verbose'] -eq $true
+
+if (-not (Test-Path $PackageArchive)) {
+    throw 'The package archive specified does not exist.'
+}
 
 if (-not (Get-Module Powerdeploy)) {
     $shouldUnload = $true
-
     Import-Module "$PSScriptRoot\..\Powerdeploy"
 }
 
-$configurationVariables = Get-ConfigurationVariable -SettingsPath $SettingsPath
-$applicableVariables = $configurationVariables | `
-    Resolve-ConfigurationVariable `
-        -EnvironmentName $EnvironmentName `
-        -ComputerName $ComputerName `
-        -AsHashTable
+$packageName = Split-Path $packageArchive -Leaf
+if (-not ($packageName -match '^(PD_)?(?<application>[^_]+)_(?<version>.+).zip$')) {
+    throw 'The package archive name is not a valid Powerdeploy package name.'
+}
 
+$applicationName = $Matches.application
+$version = $Matches.version
+
+Write-Verbose "Wrapper: Deployment requested for $applicationName version $version to $EnvironmentName."
+
+Write-Verbose "Wrapper: Retrieving configuration variables for deployment..."
+$configurationVariables = Get-ConfigurationVariable `
+    -SettingsPath $SettingsPath `
+    -EnvironmentName $EnvironmentName `
+    -ComputerName $ComputerName `
+    -ApplicationName $applicationName `
+    -Version $version `
+    -Resolve `
+    -AsHashTable `
+    -Verbose:$verbose
+
+Write-Verbose "Wrapper: The following configuration variables apply to this deployment: $(($configurationVariables.GetEnumerator() | ForEach-Object { $_.Name }) -join ', ')"
+
+Write-Verbose "Wrapper: Invoking remote deployment to $ComputerName..."
 Invoke-Powerdeploy `
     -PackageArchive $PackageArchive `
     -Environment $EnvironmentName `
     -ComputerName $ComputerName `
-    -Variable $applicableVariables `
-    -Verbose
+    -Variable $configurationVariables `
+    -Verbose:$verbose
+
+Write-Verbose "Wrapper: The remove deployment completed."
 
 if ($shouldUnload) {
+    Write-Verbose "Wrapper: Unloading Powerdeploy module."
     Remove-Module Powerdeploy
 }
